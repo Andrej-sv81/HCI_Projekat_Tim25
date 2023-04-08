16ffc1cd0d55527ff31f267bc2603f3bee9827f0
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-04-08 22:12:52 +0200

    Project creation

---
+++ HCI_Tim25/src/main/java/com/HCI/back/HCI_Tim25/HciTim25Application.java
@@ -0,0 +1,13 @@
+package com.HCI.back.HCI_Tim25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HciTim25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HciTim25Application.class, args);
+	}
+
+}
---
+++ HCI_Tim25/src/test/java/com/HCI/back/HCI_Tim25/HciTim25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HCI.back.HCI_Tim25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HciTim25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
