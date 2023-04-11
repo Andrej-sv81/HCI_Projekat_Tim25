b69314f7b878e1b08ce025d2cd1bf0c8c4f93762
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-04-11 06:11:13 +0200

    Added a leaflet map to the details page

--- angular-hci-mini-project/src/app/details/details.component.ts
+++ angular-hci-mini-project/src/app/details/details.component.ts
@@ -1,18 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { Country } from '../country-details-model';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements AfterViewInit{
+
   @Input() deviceXs!: boolean;
   @Input() country!: Country;
   @Output("closeDetails") closeDetails: EventEmitter<any> = new EventEmitter();
-  close(){
 
+  ngAfterViewInit(): void{
+      this.initMap();
   }
+
+
+  initMap() {
+    const map = L.map('mapContainer')
+    .setView([this.country.latitude, this.country.longitude], 6);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    .addTo(map);
+  }
+
+
 
   onCloseDetails(){
     this.closeDetails.emit();
